5836f294d095dcfd89c99166d05835840c787db5
Abir132 <abirhossen422@gmail.com>
2020-08-30 00:43:08 -0700

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <link href="{{ asset('css/app.css') }}" ref="stylesheet">
+
+    </head>
+    <body>
+    <div class="card">
+        <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+    </body>
+</html>
